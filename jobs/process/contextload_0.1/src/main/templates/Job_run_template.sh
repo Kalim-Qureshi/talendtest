315e3e83e2a2f4e61bb5cbff46a84034d2d074fc
NirbhayT-Continuum <nirbhay.thakkar@continuum.net>
2018-06-27 15:25:08 +0530

    Git commit again

--- jobs/process/contextload_0.1/src/main/templates/Job_run_template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/ini4j-0.5.1.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/contextload_0_1.jar: local_project.contextload_0_1.Contextload  "$@" 
--- jobs/process/demo/onboardingdemojob_0.1/src/main/templates/Job_run_template.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;onboardingdemojob_0_1.jar; local_project.onboardingdemojob_0_1.OnBoardingDemoJob  --context=Default %* 
