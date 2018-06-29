f6c4aef6f0f29b2c799bfdf1350830a347b6998a
NirbhayT-Continuum <nirbhay.thakkar@continuum.net>
2018-06-29 17:10:46 +0530

    New git commit.

--- jobs/process/contextload_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/ini4j-0.5.1.jar;../lib/log4j-1.2.16.jar;contextload_0_1.jar;' local_project.contextload_0_1.Contextload  %* 
--- jobs/process/demo/onboardingdemojob_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;onboardingdemojob_0_1.jar;' local_project.onboardingdemojob_0_1.OnBoardingDemoJob  --context=Default %* 
